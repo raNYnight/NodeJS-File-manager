0d41986a6800db6781caa7a4cc30b63f3da6feee
Ilya <rany4ka@gmail.com>
2024-02-03 15:37:50 +0300

    init: task 2, implement file manager skeleton

---
+++ index.js
@@ -0,0 +1,74 @@
+import { createInterface } from "readline";
+
+const args = process.argv.slice(2);
+const usernameArg = args.find((arg) => arg.startsWith("--username="));
+const username = usernameArg ? usernameArg.split("=")[1] : "User";
+
+console.log(`Welcome to the File Manager, ${username}!`);
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "FileManager> ",
+});
+
+rl.prompt();
+rl.on("line", (line) => {
+  const [command, ...args] = line.trim().split(" ");
+
+  switch (command) {
+    case "up":
+      navigateUp();
+      break;
+    case "cd":
+      navigateToDirectory(args[0]);
+      break;
+    case "ls":
+      listDirectoryContents();
+      break;
+    case "cat":
+      readFile(args[0]);
+      break;
+    case "add":
+      createFile(args[0]);
+      break;
+    case "rn":
+      renameFile(args[0], args[1]);
+      break;
+    case "cp":
+      copyFile(args[0], args[1]);
+      break;
+    case "mv":
+      moveFile(args[0], args[1]);
+      break;
+    case "rm":
+      deleteFile(args[0]);
+      break;
+    case "os":
+      handleOSInfo(args);
+      break;
+    case "hash":
+      calculateFileHash(args[0]);
+      break;
+    case "compress":
+      compressFile(args[0], args[1]);
+      break;
+    case "decompress":
+      decompressFile(args[0], args[1]);
+      break;
+    case ".exit":
+      exitProgram();
+      break;
+    default:
+      console.log("Invalid input");
+  }
+
+  rl.prompt();
+}).on("close", () => {
+  exitProgram();
+});
+
+function exitProgram() {
+  console.log("Exiting File Manager...");
+  rl.close();
+}
